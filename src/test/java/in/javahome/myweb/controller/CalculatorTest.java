e4a9436cdfe912af7fd99c66253203a92d2fce69
mmsandeep86 <mmsandeep86@gmail.com>
2020-07-10 22:11:15 +0530

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -2,7 +2,7 @@ package in.javahome.myweb.controller;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
-
+{
 public class CalculatorTest extends TestCase {
 	Calculator cal = new Calculator();
 	public void testAdd(){
